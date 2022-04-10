3a008dce0909f49697b9291a734961b1b040615d
Admin <ibrahimfakhreyams@gmail.com>
2022-04-10 10:16:11 +0200

    adding the educATION

--- main.py
+++ main.py
@@ -10,6 +10,36 @@ def home():
 
     return render_template("index.html")
 
+@app.route('/entertainment')
+def entertainment():
+
+    return render_template("index.html")
+@app.route('/sport')
+def sport():
+
+    return render_template("index.html")
+
+@app.route('/education')
+def education():
+
+    return render_template("edu.html")
+
+@app.route('/post_details')
+def post_details_Copy():
+
+    return render_template("post_details - Copy.html")
+@app.route('/post_details')
+def post_details():
+
+    return render_template("post_details.html")
+@app.route('/post_details')
+def post_details2():
+
+    return render_template("post_details - Copy (2).html")
+@app.route('/post_details')
+def post_details3():
+
+    return render_template("post_details - Copy (3).html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
